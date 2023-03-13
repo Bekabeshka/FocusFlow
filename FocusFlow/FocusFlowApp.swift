b3ba72ed666e0992e4d4c27c299a3ee516e3dca9
bekabeshka <4bekdaulet@gmail.com>
2023-03-13 20:59:38 +0600

    init SwiftUI iOS project

---
+++ FocusFlow/FocusFlowApp.swift
@@ -0,0 +1,17 @@
+//
+//  FocusFlowApp.swift
+//  FocusFlow
+//
+//  Created by bekabeshka on 13.03.2023.
+//
+
+import SwiftUI
+
+@main
+struct FocusFlowApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
